d00f50fe29487040bf8918e81cc9737658c52933
Gustavo Vinicius <guvinidantas@gmail.com>
2022-10-17 16:37:06 -0300

    Conectando  com database e criando o .env

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
+import "reflect-metadata";
 import Express from 'express';
-import "reflect-metadata"
+import {AppDataSource} from './database/data-source';
 import routes from './routes';
 
 const app = Express();
 
+AppDataSource.initialize().then(() => {
+    console.log('Conectado com o db');
+}).catch((err) => {
+    console.log(err);
+});
+
 app.use(Express.json());
 app.use(routes)
 
-app.listen('3080', () => {
+app.listen(process.env.SERVER_PORT, () => {
     console.log('servidor no ar!!');
 });
